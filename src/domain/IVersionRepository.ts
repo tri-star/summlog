26acdeff316ecc01c0d64929f425f568984fa030
tristar <tristar@urban-theory.net>
2020-06-04 00:58:02 +0900

    Modify comment.

--- src/domain/IVersionRepository.ts
+++ src/domain/IVersionRepository.ts
@@ -1,6 +1,9 @@
 import { Version } from "./Version";
 
-
+/**
+ * An interface for store/save version information
+ * from package.json / dedicated json file etc.
+ */
 export interface IVersionRepository {
   fetchVersion(): Version|undefined
 
